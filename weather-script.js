10322d90adbcd985563086d14527d019fb81f70c
Tommy Brader <tj.brader98@gmail.com>
2021-04-20 18:53:10 +0100

    begin js weather buttons

--- weather-script.js
+++ weather-script.js
@@ -1,4 +1,4 @@
-// Weather
+// Location Search
 function loadWeatherApp() {
   const weatherForm = document.querySelector('#weather-search form')
   weatherForm.addEventListener('submit', function(event) {
@@ -22,20 +22,6 @@ function clearWeatherInfo() {
 
 function displayWeatherInfo(data) {
   const weatherInfoSection = document.querySelector('#weather-information')
-
-  // const locationHeader = document.createElement('h2')
-  // locationHeader.textContent = data.name
-
-  // const temp = document.createElement('h6')
-  // temp.innerHTML = `Temperature: ${Math.round(data.main.temp)}&#8451;`
-
-  // const icon = document.createElement('img')
-  // icon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-
-  // weatherInfoSection.appendChild(locationHeader)
-  // weatherInfoSection.appendChild(temp)
-  // weatherInfoSection.appendChild(icon)
-
   const hoursDifferent = parseInt(data.timezone)/3600
   weatherInfoSection.innerHTML = `<h2 class="sub-sub-title">Result:</h2>
   <article id="weather-result">
@@ -61,4 +47,41 @@ function displayWeatherInfo(data) {
   </article>`
 }
 
+// Weather Buttons
+function loadWeatherAppAntarctica() {
+  const antarcticaButton = document.querySelector('#antarctica-button')
+  antarcticaButton.addEventListener('click', function(event) {
+    event.preventDefault()
+    const API_KEY = '4cd4c4999567d4caa14b0d94b58714bd'
+    const PROXY_URL = 'https://cors-anywhere.herokuapp.com'
+    const place = antarcticaButton.name
+    fetch(`${PROXY_URL}/https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${API_KEY}&units=metric`).then(function(res) {
+      return res.json()
+    }).then(function(data) {
+      clearWeatherInfo()
+      displayWeatherInfo(data)
+    })
+  })
+}
+
+function loadWeatherAppAuckland() {
+  const aucklandButton = document.querySelector('#auckland-button')
+  aucklandButton.addEventListener('click', function(event) {
+    event.preventDefault()
+    const API_KEY = '4cd4c4999567d4caa14b0d94b58714bd'
+    const PROXY_URL = 'https://cors-anywhere.herokuapp.com'
+    const place = aucklandButton.name
+    fetch(`${PROXY_URL}/https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${API_KEY}&units=metric`).then(function(res) {
+      return res.json()
+    }).then(function(data) {
+      clearWeatherInfo()
+      displayWeatherInfo(data)
+    })
+  })
+}
+
+
+//Call Functions
 loadWeatherApp()
+loadWeatherAppAntarctica()
+loadWeatherAppAuckland()
