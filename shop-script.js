24484642b2982a0f2ad1ece6581b78f4eb53445d
Tommy Brader <tj.brader98@gmail.com>
2021-04-28 17:41:31 +0100

    begin shop functionality

--- shop-script.js
+++ shop-script.js
@@ -1,74 +1,194 @@
+// Create Products and Basket variables
 const products = [
   {
-    id: 'tshirt1',
-    name: 'PIP Unisex White T-Shirt - Big Logo',
+    id: '0',
+    name: 'PIP Unisex T-Shirt - Big Logo',
     imagePath: './images/shop-tshirt.jpg',
     price: 11.99,
+    basket: 0,
   },
   {
-    id: 'tshirt2',
-    name: 'PIP Unisex White T-Shirt - Small Logo',
+    id: '1',
+    name: 'PIP Unisex T-Shirt - Small Logo',
     imagePath: './images/shop-tshirt2.jpg',
     price: 11.99,
+    basket: 0,
   },
   {
-    id: 'mug1',
+    id: '2',
     name: 'PIP Mug',
     imagePath: './images/shop-mug.jpg',
     price: 4.99,
+    basket: 0,
   },
   {
-    id: 'mug2',
+    id: '3',
     name: 'Penguin Mug',
     imagePath: './images/shop-mug2.jpg',
     price: 4.99,
+    basket: 0,
   },
   {
-    id: 'glass',
+    id: '4',
     name: 'Penguin Snowy Glass',
     imagePath: './images/shop-glass.jpg',
     price: 3.99,
+    basket: 0,
   },
   {
-    id: 'book',
+    id: '5',
     name: 'Penguin Book',
     imagePath: './images/shop-book.jpg',
     price: 4.99,
+    basket: 0,
   },
   {
-    id: 'globe',
+    id: '6',
     name: 'Miniature Globe',
     imagePath: './images/shop-globe.jpg',
     price: 4.99,
+    basket: 0,
   },
   {
-    id: 'wood',
+    id: '7',
     name: 'Wooden Penguin Ornament',
     imagePath: './images/shop-wood.jpg',
     price: 7.99,
+    basket: 0,
   },
   {
-    id: 'plastic',
+    id: '8',
     name: 'Plastic Penguin Ornament',
     imagePath: './images/shop-plastic.jpg',
     price: 6.99,
+    basket: 0,
   },
   {
-    id: 'toy',
+    id: '9',
     name: 'Penguin Toy',
     imagePath: './images/shop-toy.jpg',
     price: 3.99,
+    basket: 0,
   },
   {
-    id: 'blanket',
+    id: '10',
     name: 'Penguin Blanket (150x180cm)',
     imagePath: './images/shop-blanket.jpg',
     price: 9.99,
+    basket: 0,
   },
   {
-    id: 'poster',
+    id: '11',
     name: 'Penguin Poster (73x53cm)',
     imagePath: './images/shop-poster.jpg',
     price: 14.99,
+    basket: 0,
   },
 ]
+
+// Display Products
+function showProducts() {
+  const productsSection = document.querySelector('#products')
+  products.forEach((item) => {
+    const productArticle = document.createElement('article')
+    const productFigure = document.createElement('figure')
+    productFigure.classList.add('product')
+
+    const productName = document.createElement('figcaption')
+    productName.classList.add('product-name')
+    productName.textContent = item.name
+
+    const productImage = document.createElement('img')
+    productImage.classList.add('product-image')
+    productImage.src = item.imagePath
+    productImage.alt = item.name
+
+    const productPrice = document.createElement('figcaption')
+    productPrice.classList.add('product-price')
+    productPrice.textContent = '£' + item.price.toFixed(2)
+
+    const addToBasket = document.createElement('figcaption')
+    addToBasket.classList.add('add-to-basket')
+    const addToBasketButton = document.createElement('button')
+    addToBasketButton.classList.add('add-to-basket-button')
+    addToBasketButton.type = 'button'
+    addToBasketButton.textContent = 'Add to Basket'
+    addToBasketButton.dataset.id = item.id
+    const productID = item.id
+    addToBasketButton.addEventListener('click', addToBasketClicked)
+
+    productsSection.appendChild(productArticle)
+    productArticle.appendChild(productFigure)
+    productFigure.appendChild(productName)
+    productFigure.appendChild(productImage)
+    productFigure.appendChild(productPrice)
+    productFigure.appendChild(addToBasket)
+    addToBasket.appendChild(addToBasketButton)
+  })
+}
+
+// Basket - add, remove and increase quantity
+function addToBasketClicked(event) {
+  event.preventDefault()
+  itemID = event.target.dataset.id
+  item = products[itemID]
+  console.log(item.name + ' added to basket')
+  const basketSection = document.querySelector('#basket')
+  const basketArticle = document.createElement('article')
+  const basketFigure = document.createElement('figure')
+  basketFigure.classList.add('product-in-basket')
+
+  const basketImage = document.createElement('img')
+  basketImage.classList.add('basket-image')
+  basketImage.src = item.imagePath
+  basketImage.alt = item.name
+
+  const basketProductName = document.createElement('figcaption')
+  basketProductName.classList.add('basket-product-name')
+  basketProductName.textContent = item.name
+
+  const basketQuantity = document.createElement('input')
+  basketQuantity.classList.add('basket-quantity')
+  basketQuantity.type = 'number'
+  basketQuantity.name = 'quantity'
+  basketQuantity.defaultValue = 1
+  basketQuantity.min = 1
+  let quantity = basketQuantity.value
+  basketQuantity.addEventListener('click', function(event) {
+    event.preventDefault()
+    console.log(item.name + ' Quantity Increased')
+    quantity = basketQuantity.value
+    basketPrice.textContent = '£' + quantity * item.price
+  })
+
+  const basketPrice = document.createElement('figcaption')
+  basketPrice.classList.add('basket-price')
+  basketPrice.textContent = '£' + quantity * item.price
+
+  const basketRemove = document.createElement('button')
+  basketRemove.classList.add('basket-remove')
+  basketRemove.type = 'button'
+  basketRemove.textContent = 'REMOVE'
+  basketRemove.addEventListener('click', function(event) {
+    event.preventDefault()
+    console.log('Remove Item Clicked')
+    basketArticle.innerHTML = ''
+  })
+
+  const totalPrice = document.createElement('article')
+  totalPrice.classList.add('total-price')
+
+  basketFigure.appendChild(basketRemove)
+  basketSection.appendChild(basketArticle)
+  basketArticle.appendChild(basketFigure)
+  basketFigure.appendChild(basketImage)
+  basketFigure.appendChild(basketProductName)
+  basketFigure.appendChild(basketQuantity)
+  basketFigure.appendChild(basketPrice)
+}
+
+
+
+
+// Call Functions
+showProducts()
