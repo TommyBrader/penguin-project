0dfa8ae0a9cb5fafacbcf1bd9fb18496716cfdbc
Tommy Brader <tj.brader98@gmail.com>
2021-04-28 13:29:53 +0100

    add more shop pictures and create products list

---
+++ shop-script.js
@@ -0,0 +1,74 @@
+const products = [
+  {
+    id: 'tshirt1',
+    name: 'PIP Unisex White T-Shirt - Big Logo',
+    imagePath: './images/shop-tshirt.jpg',
+    price: 11.99,
+  },
+  {
+    id: 'tshirt2',
+    name: 'PIP Unisex White T-Shirt - Small Logo',
+    imagePath: './images/shop-tshirt2.jpg',
+    price: 11.99,
+  },
+  {
+    id: 'mug1',
+    name: 'PIP Mug',
+    imagePath: './images/shop-mug.jpg',
+    price: 4.99,
+  },
+  {
+    id: 'mug2',
+    name: 'Penguin Mug',
+    imagePath: './images/shop-mug2.jpg',
+    price: 4.99,
+  },
+  {
+    id: 'glass',
+    name: 'Penguin Snowy Glass',
+    imagePath: './images/shop-glass.jpg',
+    price: 3.99,
+  },
+  {
+    id: 'book',
+    name: 'Penguin Book',
+    imagePath: './images/shop-book.jpg',
+    price: 4.99,
+  },
+  {
+    id: 'globe',
+    name: 'Miniature Globe',
+    imagePath: './images/shop-globe.jpg',
+    price: 4.99,
+  },
+  {
+    id: 'wood',
+    name: 'Wooden Penguin Ornament',
+    imagePath: './images/shop-wood.jpg',
+    price: 7.99,
+  },
+  {
+    id: 'plastic',
+    name: 'Plastic Penguin Ornament',
+    imagePath: './images/shop-plastic.jpg',
+    price: 6.99,
+  },
+  {
+    id: 'toy',
+    name: 'Penguin Toy',
+    imagePath: './images/shop-toy.jpg',
+    price: 3.99,
+  },
+  {
+    id: 'blanket',
+    name: 'Penguin Blanket (150x180cm)',
+    imagePath: './images/shop-blanket.jpg',
+    price: 9.99,
+  },
+  {
+    id: 'poster',
+    name: 'Penguin Poster (73x53cm)',
+    imagePath: './images/shop-poster.jpg',
+    price: 14.99,
+  },
+]
